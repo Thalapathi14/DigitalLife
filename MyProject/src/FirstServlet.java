7821b03affc50e0db77f4898ef68552dc021dc52
THALAPATHI <THALAPATHI@DESKTOP-NM9UHSF>
2018-09-03 22:09:04 +0530

    refs #TempBranch web project commit

---
+++ MyProject/src/FirstServlet.java
@@ -0,0 +1,24 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+public class FirstServlet extends HttpServlet {
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		out.println("Hello world");
+		System.out.print("-------------------------------------------------------");
+	}
+	
+	
+}
